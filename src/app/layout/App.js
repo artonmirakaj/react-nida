2e0f14be9f169e0cd6aea69d967de994f7beff89
artonmirakaj <arton.mirakaj@commonbond.co>
2020-09-13 22:05:43 -0400

    add semantic ui css and dashboard component

--- src/app/layout/App.js
+++ src/app/layout/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Project Nidavellir</h1>
+        <EventDashboard />
       </div>
     );
   }
