1e12316c4ca747d24250c74ed643dec1946807bd
artonmirakaj <arton.mirakaj@commonbond.co>
2020-09-21 11:31:18 -0400

    introduction to redux

---
+++ src/features/testarea/TestActions.js
@@ -0,0 +1,13 @@
+const { INCREMENT_COUNTER, DECREMENT_COUNTER } = require('./TestConstants');
+
+export const incrementCounter = () => {
+  return {
+    type: INCREMENT_COUNTER
+  }
+}
+
+export const decrementCounter = () => {
+  return {
+    type: DECREMENT_COUNTER
+  }
+}
