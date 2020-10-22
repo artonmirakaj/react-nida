16a650766d9a21b26869e7abdb60b110441ee0ce
artonmirakaj <arton.mirakaj@commonbond.co>
2020-10-22 12:00:52 -0400

    date input

--- src/features/event/EventForm/EventForm.js
+++ src/features/event/EventForm/EventForm.js
@@ -7,6 +7,7 @@ import { Button, Form, Grid, Header, Segment } from 'semantic-ui-react';
 import TextInput from '../../../app/common/form/TextInput';
 import TextArea from '../../../app/common/form/TextArea';
 import SelectInput from '../../../app/common/form/SelectInput';
+import DateInput from '../../../app/common/form/DateInput';
 import {
   composeValidators,
   combineValidators,
@@ -85,7 +86,10 @@ class EventForm extends Component {
               />
               <Field
                 name='date'
-                component={TextInput}
+                component={DateInput}
+                dateFormat='dd LLLL yyyy h:mm a'
+                showTimeSelect
+                timeFormat='HH:mm'
                 placeholder='Event Date'
               />
 
@@ -125,6 +129,7 @@ const validate = combineValidators({
   )(),
   city: isRequired('city'),
   venue: isRequired('venue'),
+  date: isRequired('date'),
 });
 
 const mapStateToProps = (state, ownProps) => {
