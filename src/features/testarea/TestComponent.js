0e445a32f22b7b8595003b18ab2df368ae1a2775
artonmirakaj <arton.mirakaj@commonbond.co>
2020-10-31 10:25:37 -0400

    removed google api key

--- src/features/testarea/TestComponent.js
+++ src/features/testarea/TestComponent.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Button } from 'semantic-ui-react';
+import SimpleMap from './SimpleMap';
 import { incrementCounter, decrementCounter } from './TestActions';
 import TestPlaceInput from './TestPlaceInput';
+import { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
 
 class TestComponent extends Component {
+  state = {
+    latlng: {
+      lat: 59.95,
+      lng: 30.33,
+    },
+  };
+
+  handleSelect = (address) => {
+    geocodeByAddress(address)
+      .then((results) => getLatLng(results[0]))
+      .then((latLng) => {
+        this.setState({
+          latlng: latLng,
+        });
+      })
+      .catch((error) => console.error('Error', error));
+  };
+
   render() {
     const { data, incrementCounter, decrementCounter } = this.props;
     return (
@@ -16,7 +36,11 @@ class TestComponent extends Component {
         <br />
         <br />
         <br />
-        <TestPlaceInput />
+        <TestPlaceInput selectAddress={this.handleSelect} />
+        <br />
+        <br />
+        <br />
+        <SimpleMap key={this.state.latlng.lng} latlng={this.state.latlng} />
       </div>
     );
   }
